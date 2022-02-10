68b5f634d40e36b1ac2c3ab8abe50abac1af4fef
YS <yoonsam86@gmail.com>
2022-02-10 23:49:29 +0900

    initial Project

---
+++ src/test/kotlin/com/ys/spring/SpringApiSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ys.spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringApiSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
